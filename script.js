ea369cb40af32a97dce1bf103969e4bfecc5f8f1
unknown <rafael.giannico@gmail.com>
2023-09-01 19:30:51 -0300

    Assets adicionados

--- script.js
+++ script.js
@@ -1,55 +1,88 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
+const airplaneSound = document.getElementById("airplaneSound");
+const explosionSound = document.getElementById("explosionSound");
+
 let airplane = new Image();
 let missile = new Image();
-let airplaneSound = new Audio("aviao.mp3");
-let explosionSound = new Audio("explosao.mp3");
 
-let airplaneX = 0;
-let airplaneY = 0;
-let missileX = canvas.width / 2;
-let missileY = canvas.height - 50;
+let airplaneX, airplaneY;
+let missileX, missileY;
 let missileFired = false;
 let soundEnabled = true;
+
+let speed = 5;
+let closeDistance = 50;
 
 airplane.src = "aviao.png";
 missile.src = "missil.png";
 
 function toggleSound() {
     soundEnabled = !soundEnabled;
+    if (soundEnabled) {
+        airplaneSound.play();
+    } else {
+        airplaneSound.pause();
+    }
 }
+
+function resizeCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    missileX = canvas.width / 2;
+    missileY = canvas.height - 50;
+}
+
+window.addEventListener("resize", resizeCanvas);
+resizeCanvas();
 
 function update() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    if (soundEnabled) {
+    if (soundEnabled && !airplaneSound.paused) {
         airplaneSound.play();
     }
 
-    // Draw airplane and missile
     ctx.drawImage(airplane, airplaneX, airplaneY);
-    ctx.drawImage(missile, missileX, missileY);
 
-    // Fire missile
+    let delta = Math.atan((airplaneX - missileX) / (missileY - airplaneY));
+    let angleInRadians = -Math.PI / 2 + delta;
+
     if (missileFired) {
-        let dx = airplaneX - missileX;
-        let dy = airplaneY - missileY;
-        let distance = Math.sqrt(dx*dx + dy*dy);
+        if (missileY - airplaneY < 0) {
+            angleInRadians += -Math.PI;
+        }
 
-        missileX += dx / distance;
-        missileY += dy / distance;
+        if (missileY - airplaneY < 0) {
+            missileX -= speed * Math.sin(delta);
+            missileY += speed * Math.cos(delta);
+        } else {
+            missileX += speed * Math.sin(delta);
+            missileY -= speed * Math.cos(delta);
+        }
 
-        if (distance < 50) {
+        let distance = Math.sqrt(Math.pow(airplaneX - missileX, 2) + Math.pow(airplaneY - missileY, 2));
+
+        if (distance < closeDistance) {
             if (soundEnabled) {
                 explosionSound.play();
             }
+
             // Reset
             missileFired = false;
             missileX = canvas.width / 2;
             missileY = canvas.height - 50;
+            airplaneX = canvas.width / 2;
+            airplaneY = 50;
         }
     }
+
+    ctx.save();
+    ctx.translate(missileX, missileY);
+    ctx.rotate(angleInRadians);
+    ctx.drawImage(missile, -missile.width / 2, -missile.height / 2);
+    ctx.restore();
 
     requestAnimationFrame(update);
 }
